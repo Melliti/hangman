aa46e9ec3d8ed11ba5b4d14f37f232de2f672fe4
melliti <ramzi.melliti@epitech.eu>
2018-12-05 12:40:53 +0100

    [Add] word with hidden characters

--- js/hangman.js
+++ js/hangman.js
@@ -1,4 +1,31 @@
+var pokemonInfo = new Array;
+var pokemonName = "";
+
+function formatStr(pokemonName) {
+   var a = [], start=0;
+   while(start<pokemonName.length) {
+      a.push(pokemonName.slice(start, start+1));
+      start+=1;
+   }
+   return a.join(" ");
+}
+
+function createHangman() {
+    var i = 0;
+
+    while (i != pokemonInfo[0].length - 1)
+    {
+        pokemonName += '_';
+        i++;
+    }
+    pokemonName = pokemonName.replace(pokemonName.charAt(0), pokemonInfo[0][0]);
+    pokemonName = pokemonName.replace(/.$/, pokemonInfo[0][i]);
+    //pokemonName[i - 1] = pokemonInfo[0][i - 1];
+    console.log(pokemonName);
+}
+
 function randomPokemon() {
+    // 802 = number of pokemon in the API
     var selectedPokemon = 0;
     selectedPokemon = Math.floor((Math.random() * 802) + 1);
     return selectedPokemon;
@@ -6,7 +33,7 @@ function randomPokemon() {
 
 function pickPokemon() {
     var reader = new XMLHttpRequest();
-    var pokemonInfo = new Array;
+    var divName = document.getElementById('name');
     reader.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + randomPokemon() + '/', true);
     reader.send('null');
     reader.onreadystatechange = function () {
@@ -18,6 +45,9 @@ function pickPokemon() {
                 pokemonInfo[0] = pokemonObj.name;
                 pokemonInfo[1] = pokemonObj.sprites.front_default;
                 pokemonInfo[2] = pokemonObj.sprites.front_shiny;
+                createHangman();
+                pokemonName = formatStr(pokemonName);
+                divName.innerHTML = '<p>' + pokemonName + '</p>';
                 return pokemonInfo;
             }
         }
